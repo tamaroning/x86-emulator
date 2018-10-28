2749f361be146dfc0c0d66a6409c91e7c333796d
simauma1203 <20992019+simauma1203@users.noreply.github.com>
2018-10-28 11:34:04 +0900

    add lst folder

--- io/!cons_nt.bat
+++
@@ -1,4 +0,0 @@
-path C:\Windows;C:\Windows\System32;%~dp0..\z_tools;%~dp0..\z_tools\i386-elf-gcc\bin;%~dp0..\z_tools\mingw32-gcc\bin
-%~d0
-cd %~dp0
-cmd.exe
--- io/in.asm
+++
@@ -1,5 +0,0 @@
-BITS 32
-    org 0x7c00
-    mov edx, 0x03f8
-    in al, dx
-    jmp 0
